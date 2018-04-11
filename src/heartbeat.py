03f0c7b4af526feab7bc892e122cfec1f9a57b86
Brian Atwood <batwood011@gmail.com>
2018-04-11 13:34:51 -0500

    uncomment

--- src/heartbeat.py
+++ src/heartbeat.py
@@ -4,28 +4,28 @@ from src.notifications import slack, email, sms
 
 def start(config):
 	print('Waiting', config.startup_delay, 'seconds ...')
-	# time.sleep(config.startup_delay) 
+	time.sleep(config.startup_delay) 
 	print('Starting heartbeat ...')
 	consecutive_failures = 0
 
-	# while consecutive_failures < config.failures:
-	# 	print('Checking ...')
-	# 	try:
-	# 		response = requests.get(url=config.endpoint, timeout=config.timeout)
+	while consecutive_failures < config.failures:
+		print('Checking ...')
+		try:
+			response = requests.get(url=config.endpoint, timeout=config.timeout)
 
-	# 		if response.status_code == 200:
-	# 			consecutive_failures = 0
-	# 			print('Success')
-	# 		else:
-	# 			consecutive_failures += 1
-	# 			print('Fail')
+			if response.status_code == 200:
+				consecutive_failures = 0
+				print('Success')
+			else:
+				consecutive_failures += 1
+				print('Fail')
 
-	# 	except Exception as e:
-	# 		consecutive_failures += 1
-	# 		print('Fail')
+		except Exception as e:
+			consecutive_failures += 1
+			print('Fail')
 
-	# 	if consecutive_failures < config.failures:
-	# 		time.sleep(config.interval)
+		if consecutive_failures < config.failures:
+			time.sleep(config.interval)
 
 	send_notifications(config)
 
